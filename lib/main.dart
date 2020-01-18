8fd72eb3178fbea0583952305ecc929eccc446c0
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-01-18 11:52:30 +0800

    Using Row - multiple text

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,12 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('This is title'),
         ),
-        body: Center(
-          child: Container(
-            child: Image.network('https://images.unsplash.com/photo-1532465614-6cc8d45f647f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'),
-          ),
-        ),
+        body: Row(
+          children: <Widget>[
+            Text('First'),
+            Text('Second'),
+          ],
+        )
       ),
     );
   }
