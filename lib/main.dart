5314cd18355d2efbccf4d03c47867f708528db08
MUHAMMAD TARMIZI SANUSI AFKAR <realtarmizisanusi@gmail.com>
2020-01-18 12:48:57 +0800

    Home Page widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hello_world/icon-widgets.dart';
+
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
@@ -15,22 +16,76 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('This is title'),
         ),
-        body: HomePage(),
+        body: HomePageAndroid(),
       ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePageIOS extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: RaisedButton(
+      onPressed: () {
+        showDialog(
+          context: context,
+          // return object of type AlertDialog
+          builder: (BuildContext context) => CupertinoAlertDialog(
+            title: Column(
+              children: <Widget>[
+                Text("CupertinoAlertDialog"),
+                Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ],
+            ),
+            content: Text("An iOS-style alert dialog."),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text("OK"))
+            ],
+          ),
+        );
+      },
+      child: Text('Show Cupertino AlertDialog'),
+    ));
+  }
+}
+
+// Home for Android
+class HomePageAndroid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: RaisedButton(
-        child: Text('Open Alert'), 
-        onPressed: (){
-          print('Open Alert Box');
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              // return object of type AlertDialog
+              return AlertDialog(
+                title: new Text("AlertDialog"),
+                content: new Text("Creates an alert dialog."),
+                actions: <Widget>[
+                  // usually buttons at the bottom of the dialog
+                  new FlatButton(
+                    child: new Text("Close"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
         },
+        child: Text('Show AlertDialog'),
       ),
     );
   }
